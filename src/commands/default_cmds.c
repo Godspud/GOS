3159c88a680258ef29ab4f88c30dcf47663d1455
god_spud <beamongus.yt@gmail.com>
2026-03-17 11:35:48 +0800

    Update: 2026-03-17 11:35

--- src/commands/default_cmds.c
+++ src/commands/default_cmds.c
@@ -115,10 +115,10 @@ static void cmd_time(int argc, char **argv)
     print_string("Current time: ", COLOR_LIGHT_CYAN);
     print_char((cmos_current_time.hours / 10) + '0', COLOR_LIGHT_CYAN);
     print_char((cmos_current_time.hours % 10) + '0', COLOR_LIGHT_CYAN);
-    print_char(':', COLOR_LIGHT_CYAN);
+    print_char(':', COLOR_RED);
     print_char((cmos_current_time.minutes / 10) + '0', COLOR_LIGHT_CYAN);
     print_char((cmos_current_time.minutes % 10) + '0', COLOR_LIGHT_CYAN);
-    print_char(':', COLOR_LIGHT_CYAN);
+    print_char(':', COLOR_BLUE);
     print_char((cmos_current_time.seconds / 10) + '0', COLOR_LIGHT_CYAN);
     print_char((cmos_current_time.seconds % 10) + '0', COLOR_LIGHT_CYAN);
     print_char('\n', COLOR_LIGHT_CYAN);
