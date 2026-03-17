4ed18bd23f01eba860566992bc2371871d82fa2c
god_spud <beamongus.yt@gmail.com>
2026-03-17 14:39:22 +0800

    Update: 2026-03-17 14:39

--- src/cmos.c
+++ src/cmos.c
@@ -36,8 +36,8 @@ void cmos_get_time(cmos_time *time)
     // Wait until the CMOS is not updating to ensure we get a consistent time reading
     while (cmos_read(0x0A) & 0x80)
         ;
-    second = 100; // cmos_read(0x32);
-    minute = 10;  // cmos_read(0x02);
+    second = cmos_read(0x00);
+    minute = cmos_read(0x02);
     hour = cmos_read(0x04);
     day = cmos_read(0x07);
     month = cmos_read(0x08);
