f57d0c4911c070e3805ddb7a0aa1a5378e6379b5
god_spud <beamongus.yt@gmail.com>
2026-03-17 11:34:41 +0800

    Update: 2026-03-17 11:34

--- src/cmos.c
+++ src/cmos.c
@@ -1,5 +1,6 @@
 #include "cmos.h"
 #include "io.h"
+#define CURRENT_YEAR 2023
 
 cmos_time cmos_current_time;
 
@@ -7,12 +8,65 @@ void cmos_init(void)
 {
     // Nothing to initialize for CMOS
 }
-
+unsigned char cmos_read(unsigned char reg)
+{
+    outb(reg, 0x70);
+    return inb(0x71);
+}
+static unsigned char bcd_to_bin(unsigned char bcd)
+{
+    return ((bcd & 0x0F) + ((bcd >> 4) * 10));
+}
 /**
  * cmos_get_time: Reads the current time from the CMOS registers and fills the provided cmos_time structure with the values. The function reads the seconds, minutes, hours, day, month, year, and century from their respective CMOS registers (0x00 to 0x09 and 0x32) and stores them in the cmos_time structure pointed to by the time parameter.
  */
 void cmos_get_time(cmos_time *time)
 {
+    unsigned char century;
+    unsigned char second;
+    unsigned char minute;
+    unsigned char hour;
+    unsigned char day;
+    unsigned char month;
+    unsigned char year;
+    unsigned char registerB;
+    // Wait until the CMOS is not updating to ensure we get a consistent time reading
+    while (cmos_read(0x0A) & 0x80)
+        ;
+    second = cmos_read(0x00);
+    minute = cmos_read(0x02);
+    hour = cmos_read(0x04);
+    day = cmos_read(0x07);
+    month = cmos_read(0x08);
+    year = cmos_read(0x09);
+    century = cmos_read(0x32);
+    if (!(registerB & 0x04))
+    {
+        second = (second & 0x0F) + ((second / 16) * 10);
+        minute = (minute & 0x0F) + ((minute / 16) * 10);
+        hour = ((hour & 0x0F) + (((hour & 0x70) / 16) * 10)) | (hour & 0x80);
+        day = (day & 0x0F) + ((day / 16) * 10);
+        month = (month & 0x0F) + ((month / 16) * 10);
+        year = (year & 0x0F) + ((year / 16) * 10);
+        if (century != 0)
+        {
+            century = (century & 0x0F) + ((century / 16) * 10);
+        }
+    }
+    if (!(registerB & 0x02) && (hour & 0x80))
+    {
+        hour = ((hour & 0x7F) + 12) % 24;
+    }
+    if (century != 0)
+    {
+        year += century * 100;
+    }
+    else
+    {
+        year += (CURRENT_YEAR / 100) * 100;
+        if (year < CURRENT_YEAR)
+            year += 100;
+    }
     // Read time values from CMOS registers found at https://osdev.wiki/wiki/%601%60#Getting_Current_Date_and_Time_from_RTC
     time->seconds = inb(0x00);
     time->minutes = inb(0x02);
