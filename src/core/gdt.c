bc12893de14a5aaba20b4c9e1ecaa58a9fb0c7c9
god_spud <beamongus.yt@gmail.com>
2026-03-31 20:14:53 +0800

    Update: 2026-03-31 20:14

--- src/core/gdt.c
+++ src/core/gdt.c
@@ -1,6 +1,6 @@
 __attribute__((noreturn)) void exception_handler(void);
 void exception_handler()
 {
-    __asm__ volatile("cli; hlt");
-    // Completely hangs the computer
+    __asm__ volatile("cli; hlt;");
+    while (1); // Ensure the function never returns
 }
