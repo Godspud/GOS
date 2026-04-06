2e87e9865802e0e390c22c2e2d5b8283d46090a2
god_spud <beamongus.yt@gmail.com>
2026-04-06 19:09:13 +0800

    Update: 2026-04-06 19:09

--- src/usr/snake.c
+++ src/usr/snake.c
@@ -1,7 +1,8 @@
 #include "vga.h"
 #include "include/drivers/keyboard.h"
 
-/*helper func
+/**
+ * helper func
  */
 void snake_redraw(int snake_pos[2])
 {
@@ -9,9 +10,26 @@ void snake_redraw(int snake_pos[2])
     vga_write_char(snake_pos[0], snake_pos[1], '#', COLOR_GREEN);
 }
 
+/**
+ * assign the last tail pos recursively
+ */
+void snake_tail_update(int snake_pos[2], int tail_pos[100][2])
+{
+    for (int counter = 99; counter > 0; counter--)
+    {
+        tail_pos[counter][0] = tail_pos[counter - 1][0];
+        tail_pos[counter][1] = tail_pos[counter - 1][1];
+    }
+    tail_pos[0][0] = snake_pos[0];
+    tail_pos[0][1] = snake_pos[1];
+}
+
 void snake_main()
 {
     int snake_pos[2] = {0, 0};
+    int tail_pos[100][2] = {0};
+    vga_clear(COLOR_BLACK);
+    vga_write_char(snake_pos[0], snake_pos[1], '#', COLOR_GREEN);
     while (1)
     {
         char key = keyboard_read(0);
@@ -21,21 +39,25 @@ void snake_main()
         }
         else if (key == 'w')
         {
+            snake_tail_update(snake_pos, tail_pos);
             snake_pos[1] -= 1;
             snake_redraw(snake_pos);
         }
         else if (key == 's')
         {
+            snake_tail_update(snake_pos, tail_pos);
             snake_pos[1] += 1;
             snake_redraw(snake_pos);
         }
         else if (key == 'a')
         {
+            snake_tail_update(snake_pos, tail_pos);
             snake_pos[0] -= 1;
             snake_redraw(snake_pos);
         }
         else if (key == 'd')
         {
+            snake_tail_update(snake_pos, tail_pos);
             snake_pos[0] += 1;
             snake_redraw(snake_pos);
         }
