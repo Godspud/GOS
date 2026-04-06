482b325085d6ecccb0703b55b726925555ee524f
god_spud <beamongus.yt@gmail.com>
2026-04-06 20:33:18 +0800

    Update: 2026-04-06 20:33

--- src/usr/snake.c
+++ src/usr/snake.c
@@ -4,10 +4,16 @@
 /**
  * helper func
  */
-void snake_redraw(int snake_pos[2])
+void snake_redraw(int snake_pos[2], int tail_pos[100][2])
 {
     vga_clear(COLOR_BLACK);
-    vga_write_char(snake_pos[0], snake_pos[1], '#', COLOR_GREEN);
+
+    for (int counter = 100; counter > 0; counter--)
+    {
+        vga_write_char(tail_pos[counter][0], tail_pos[counter][1], '@', COLOR_LIGHT_GREEN);
+    }
+
+    vga_write_char(snake_pos[0], snake_pos[1], '#', COLOR_LIGHT_GREEN);
 }
 
 /**
@@ -28,42 +34,45 @@ void snake_main()
 {
     int snake_pos[2] = {0, 0};
     int tail_pos[100][2] = {0};
+    tail_pos[0][0] = snake_pos[0];
     vga_clear(COLOR_BLACK);
-    vga_write_char(snake_pos[0], snake_pos[1], '#', COLOR_GREEN);
+    vga_write_char(snake_pos[0], snake_pos[1], '#', COLOR_LIGHT_GREEN);
     while (1)
     {
         char key = keyboard_read(0);
         if (key == '\b')
         {
+            vga_clear(COLOR_BLACK);
             break;
         }
         else if (key == 'w')
         {
-            snake_tail_update(snake_pos, tail_pos);
             snake_pos[1] -= 1;
-            snake_redraw(snake_pos);
+            snake_tail_update(snake_pos, tail_pos);
+            snake_redraw(snake_pos, tail_pos);
         }
         else if (key == 's')
         {
-            snake_tail_update(snake_pos, tail_pos);
             snake_pos[1] += 1;
-            snake_redraw(snake_pos);
+            snake_tail_update(snake_pos, tail_pos);
+            snake_redraw(snake_pos, tail_pos);
         }
         else if (key == 'a')
         {
-            snake_tail_update(snake_pos, tail_pos);
             snake_pos[0] -= 1;
-            snake_redraw(snake_pos);
+            snake_tail_update(snake_pos, tail_pos);
+            snake_redraw(snake_pos, tail_pos);
         }
         else if (key == 'd')
         {
-            snake_tail_update(snake_pos, tail_pos);
             snake_pos[0] += 1;
-            snake_redraw(snake_pos);
+            snake_tail_update(snake_pos, tail_pos);
+            snake_redraw(snake_pos, tail_pos);
         }
         // kill logic
         if ((snake_pos[0] < 0 || snake_pos[0] >= 80) || (snake_pos[1] < 0 || snake_pos[1] >= 25))
         {
+            vga_clear(COLOR_BLACK);
             break;
         }
     }
