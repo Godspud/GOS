fc8f8deff261a74f0f97babaec166a17de5ce3a6
god_spud <beamongus.yt@gmail.com>
2026-04-09 18:21:52 +0800

    Update: 2026-04-09 18:21

--- src/usr/snake.c
+++ src/usr/snake.c
@@ -92,12 +92,10 @@ void snake_main()
     tail_pos[0][0] = snake_pos[0];
     vga_clear(COLOR_BLACK);
     vga_write_char(snake_pos[0], snake_pos[1], '#', COLOR_LIGHT_GREEN);
+    move_snake(direction, snake_pos, tail_pos, food_pos, tail_length);
     while (1)
     {
-        every(1000)
-        {
-            move_snake(direction, snake_pos, tail_pos, food_pos, tail_length);
-        }
+
         char key = keyboard_read(0);
         if (key == '\b')
         {
