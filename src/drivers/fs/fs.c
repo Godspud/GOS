7fac76cd191f038db0fbfb6e7df135bf38de1024
god_spud <beamongus.yt@gmail.com>
2026-04-22 19:36:47 +0800

    Update: 2026-04-22 19:36

--- src/drivers/fs/fs.c
+++ src/drivers/fs/fs.c
@@ -1,5 +1,6 @@
 #include "include/drivers/fs/fs.h"
 #include "include/drivers/ata.h"
+#include <string.h>
 
 typedef struct
 {
@@ -23,28 +24,10 @@ void fs_init(void)
 {
     fs_header_t header;
     header.version = 1;
-    header.magic[0] = '#';
-    header.magic[1] = 'q';
-    header.magic[2] = 'o';
-    header.magic[3] = 'r';
-    header.magic[4] = 'k';
-    header.magic[5] = 'c';
-    header.magic[6] = 'r';
-    header.magic[7] = 'a';
-    header.magic[8] = 's';
-    header.magic[9] = 't';
-    header.magic[10] = 'i';
-    header.magic[11] = 'n';
-    header.magic[12] = 'a';
-    header.magic[13] = 't';
-    header.magic[14] = 'i';
-    header.magic[15] = 'n';
-    header.magic[16] = 'g';
-    header.magic[17] = 'F';
-    header.magic[18] = 'S';
+    //strcpy(header.magic, "#qorkcrastinatingFS");
     for (int i = 19; i < 512; i++)
     {
         header.reserved[i - 19] = 0;
     }
-    ata_write_sector(1, (unsigned char *)&header);
+    ata_write_sector(0, (unsigned char *)&header);
 }
