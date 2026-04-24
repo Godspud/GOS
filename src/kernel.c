eca87567d82ad6baece4276972c0d4bc145cb1c5
god_spud <beamongus.yt@gmail.com>
2026-04-24 13:32:06 +0800

    Update: 2026-04-24 13:32

--- src/kernel.c
+++ src/kernel.c
@@ -23,6 +23,7 @@ void kernel_main()
 
     // vga_clear(COLOR_BLACK);
     char buffer[512];
+    memset(buffer, 0, sizeof(buffer));
     ata_write_sector(1, &buffer); // Clear sector 1 for testing
     fs_create_file("test", "txt", "Hello, World!", 13);
     vga_enable_cursor();
